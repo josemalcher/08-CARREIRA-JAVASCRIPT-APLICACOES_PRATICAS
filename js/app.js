107bbe924b21dccb40e0f431d5ed685ad664a7c6
José Stélio Malcher Jr <contato@josemalcher.net>
2020-06-09 08:58:37 -0300

    05 - Jogo 21 JavaScript Alternar Player

--- js/app.js
+++ js/app.js
@@ -16,3 +16,28 @@ let scores1 = document.getElementById("pscores1");
 let scores2 = document.getElementById("pscores2");
 
 let letterSelectdD = document.getElementById('letter-selected');
+
+function changePlayer() {
+    playerActive = (playerActive == 1) ? 2 : 1;
+    if (playerActive == 1) {
+        play1D.classList.add('active');
+        play2D.classList.remove('active');
+    } else {
+        play2D.classList.add('active');
+        play1D.classList.remove('active');
+    }
+}
+
+function selectLetter(event) {
+    letterSelected = Math.floor((Math.random() * 13) + 1);
+    console.log(letterSelected);
+    /* Tira o efeito do link */
+    event = event || window.event;
+    if (event.preventDefault) {
+        event.preventDefault()
+    }
+    if (event.returnValue) {
+        event.returnValue();
+    }
+    return false
+}
